8aa5d7ab85efd24882361d1351721fd4dbd35e58
kkm <macovconst@yandex.kz>
2024-12-21 22:31:40 +0200

    add createCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
 import { initialCards } from './cards.js';
 
 // @todo: Темплейт карточки
-let cardTemplate = document.querySelector('#card-template').content;
-let placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
 
 // @todo: Функция удаления карточки
-let deleteCard = (element, parent) => element.closest(parent).remove();
+const deleteCard = (element, parent) => element.closest(parent).remove();
 
 // @todo: DOM узлы
 let card = cardTemplate.querySelector('.places__item');
 
 // @todo: Функция создания карточки
-
-// @todo: Вывести карточки на страницу
-initialCards.forEach((item) => {
-
+const createCard = (item) => {
   let newCard = card.cloneNode(true);
   let cardImage = newCard.querySelector('.card__image');
   let cardTitle = newCard.querySelector('.card__title');
   let cardButton = newCard.querySelector('.card__delete-button');
 
   cardImage.src = item.link;
+  cardImage.alt = `Картинка с ${item.name}`;
   cardTitle.textContent = item.name;
   cardButton.addEventListener('click', () => deleteCard(cardButton, '.card'));
-  placesList.append(newCard);
+
+  return newCard;
+}
+
+// @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placesList.append(createCard(item));
 })
