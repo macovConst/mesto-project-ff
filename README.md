e41345267ed443a3ed034a7c82d8afbe924fb0b1
kkm <macovconst@yandex.kz>
2024-12-21 19:05:44 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+  https://github.com/macovConst/mesto-project-ff
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
+let cardTemplate = document.querySelector('#card-template').content;
+let placesList = document.querySelector('.places__list');
+
+
+// @todo: Функция удаления карточки
+let deleteCard = (element, parent) => element.closest(parent).remove();
 
 // @todo: DOM узлы
+let card = cardTemplate.querySelector('.places__item');
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+
+  let newCard = card.cloneNode(true);
+  let cardImage = newCard.querySelector('.card__image');
+  let cardTitle = newCard.querySelector('.card__title');
+  let cardButton = newCard.querySelector('.card__delete-button');
+
+  cardImage.src = item.link;
+  cardTitle.textContent = item.name;
+  cardButton.addEventListener('click', () => deleteCard(cardButton, '.card'));
+  placesList.append(newCard);
+})
